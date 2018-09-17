3691669f0acc4bdda9430c9034fdfcc424835166
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-09-17 14:28:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,12 @@ class Application
     resp= Rack::Response.new
     time= Time.now
     
-    
-    
+    if time.hour >= 12
+      resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Morning!"
+    end
+    resp.finish
   end
   
 end
